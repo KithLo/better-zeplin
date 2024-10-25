e45779fe56bb43e066ffda439e2aeb3317d2e782
Kith Lo <kithlo@openrice.com>
2024-10-25 16:14:15 +0800

    Update extension

--- extension/background.js
+++ extension/background.js
@@ -14,7 +14,88 @@ async function copyUserToken(tabUrl) {
   }
 }
 
+const screenUrlRegex = /^https:\/\/app.zeplin.io\/project\/(.*?)\/screen\/(.*)$/
+
+async function fetchZeplinProject(url) {
+  const token = await chrome.cookies.get({
+    url: "https://zeplin.io",
+    name: "userToken",
+  })
+  if (!token.value) {
+    console.log("cannot get zeplin token")
+    return
+  }
+  const resp1 = await fetch(url, { method: "HEAD" })
+  const urlMatch = screenUrlRegex.exec(resp1.url)
+  if (!urlMatch) {
+    console.log("cannot get expanded url")
+    return
+  }
+  const projectId = urlMatch[1]
+  const screenId = urlMatch[2]
+  const resp2 = await fetch(`https://api.zeplin.io/v7/projects/${projectId}`, {
+    headers: {
+      "zeplin-token": token.value,
+    },
+  })
+  if (!resp2.ok) {
+    console.log("cannot get projects")
+    return
+  }
+  const project = await resp2.json()
+  const screen = project.screens.find((r) => r._id === screenId)
+  if (!screen) {
+    console.log("cannot find screen")
+    return
+  }
+  const snapshot = screen.latestVersion?.snapshot?.url
+  if (!snapshot) {
+    console.log("cannot find snapshot")
+    return
+  }
+  chrome.tabs.create({ url: snapshot })
+}
+
 const title = "Better Zeplin"
+
+function toggleRedirectRule(enable) {
+  chrome.declarativeNetRequest.updateEnabledRulesets({
+    [enable ? "enableRulesetIds" : "disableRulesetIds"]: [
+      "zelin_redirect_rules",
+    ],
+  })
+  chrome.contextMenus.update("redirect", {
+    title: enable ? "Auto redirect enabled" : "Auto redirect disabled",
+  })
+}
+
+function createContextMenu() {
+  chrome.contextMenus.create({
+    id: "redirect",
+    title: "Auto redirect disabled",
+    contexts: ["action"],
+    type: "checkbox",
+  })
+
+  chrome.contextMenus.create({
+    id: "image",
+    title: "Open as image",
+    contexts: ["link"],
+  })
+
+  chrome.contextMenus.onClicked.addListener((info) => {
+    if (info.menuItemId === "redirect") {
+      toggleRedirectRule(info.checked)
+      return
+    } else if (info.menuItemId === "image") {
+      fetchZeplinProject(info.linkUrl)
+    }
+  })
+}
+
+chrome.contextMenus.removeAll(() => {
+  createContextMenu()
+})
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (
@@ -28,7 +109,13 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 function gotoZeplin() {
   const pathname = window.location.pathname
   if (/^\/project\/.*?\/screen\//.test(pathname)) {
-    window.location.assign("https://app.zeplin.io" + window.location.pathname)
+    window.location.assign(
+      "https://" +
+        (window.location.host.includes("better-zeplin")
+          ? "app.zeplin.io"
+          : "better-zeplin.kithlo.com") +
+        window.location.pathname,
+    )
   }
 }
 
